0f73e7ac6d06799b607e544ce4021195a5277b73
Daniel Garcia <dangarpa@gmail.com>
2022-05-29 10:44:28 -0500

    randomCards

---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Card = ( {userRandom,colorRandom, clickButton} ) => {
+
+  const styleButton = {
+    backgroundColor: colorRandom
+  }
+
+
+  
+    console.log(userRandom)
+    console.log(colorRandom)
+  
+    return (
+    <article className='card' style={{color: colorRandom}}>
+        <h2 className='card__title'> {userRandom.quote} </h2>
+        <ul className='card__list '>
+          <li className='card__list'><b className='card__b'>Author: </b>
+           {userRandom.author}
+            </li>
+        </ul>
+        <button style= {styleButton} 
+        className='card__btn'
+        onClick={clickButton}
+        >&#62;</button>
+    </article>
+  )
+}
+
+export default Card
